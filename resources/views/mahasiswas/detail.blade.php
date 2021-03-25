1817f0df062ddc4aea52253a7e9bdcbc200cabf0
pramudyapolinema <1941720054@student.polinema.ac.id>
2021-03-25 11:16:12 +0700

    menambahkan email dan tanggal lahir pada detail

--- resources/views/mahasiswas/detail.blade.php
+++ resources/views/mahasiswas/detail.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center">
-        <div class="card" style="width: 24rem;">
+        <div class="card" style="width: 30rem;">
             <div class="card-header">
                 Detail Mahasiswa
             </div>
@@ -14,6 +14,8 @@
                     <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->Kelas}}</li>
                     <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->Jurusan}}</li>
                     <li class="list-group-item"><b>No_Handphone: </b>{{$Mahasiswa->No_Handphone}}</li>
+                    <li class="list-group-item"><b>Email: </b>{{$Mahasiswa->email}}</li>
+                    <li class="list-group-item"><b>Tanggal Lahir: </b>{{$Mahasiswa->tanggal_lahir}}</li>
                 </ul>
             </div>
             <a class="btn btn-success mt3" href="{{ route('mahasiswas.index') }}">Kembali</a>
